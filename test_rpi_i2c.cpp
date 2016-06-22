523d589e2d07e11db3ff3980175d6cddf8cd5dfb
Chengtao Wang <ctwang28@gmail.com>
2016-06-22 12:08:53 -0400

    Add files via upload

---
+++ test_rpi_i2c.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stdio.h>
+#include<fcntl.h>
+#include<sys/ioctl.h>
+#include<linux/i2c.h>
+#include<linux/i2c-dev.h>
+#include<iomanip>
+using namespace std;
+
+#define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
+#define DEVID       0x00
+#define BUFFER_SIZE 40
+
+int main(){
+   int file;
+   cout << "Starting the test application" << endl;
+   if((file=open("/dev/i2c-1", O_RDWR)) < 0){
+      cout << "failed to open the bus" << endl;
+      return 1;
+   }
+   if(ioctl(file, I2C_SLAVE, 0x28) < 0){
+      cout << "Failed to connect to the sensor" << endl;
+      return 1;
+   }
+   char writeBuffer[1] = {0x00};
+   if(write(file, writeBuffer, 1)!=1){
+      cout << "Failed to reset the read address" << endl;
+      return 1;
+   }
+   char readBuffer[BUFFER_SIZE];
+   if(read(file, readBuffer, BUFFER_SIZE)!=BUFFER_SIZE){
+      cout << "Failed to read in the buffer" << endl;
+      return 1;
+   }
+   cout << "The Device ID is: " << HEX(readBuffer[DEVID]) << endl;
+   close(file);
+   return 0;
+}
